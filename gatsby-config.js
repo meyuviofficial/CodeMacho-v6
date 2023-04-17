980f3bde5d786aaf59ed7d2b71bbece651b105f4
Yuvi <Yuvaraj.Selvarajan@shell.com>
2023-04-17 18:50:06 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `code-macho-v6`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
