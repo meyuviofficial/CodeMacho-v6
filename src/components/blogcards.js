1f7821afdcd9a743804b1c7642c73fc327e8878e
Yuvi <Yuvaraj.Selvarajan@shell.com>
2023-04-28 13:00:41 +0530

    fixed the ui resizing issue

--- src/components/blogcards.js
+++ src/components/blogcards.js
@@ -1,31 +1,32 @@
 import React from "react";
+import { Link } from "gatsby";
 
-const BlogCards = ({blogtitle, content, date, slug}) => {
+const BlogCards = ({ blogtitle, content, date, slug }) => {
   return (
-    <article className="w-80 h-96 border-2 border-red-600 object-center overflow-hidden rounded-2xl shadow-2xl transition hover:shadow-lg">
-      <img
-        alt="Office"
-        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-        className="h-56 w-full object-cover"
-      />
+    <div className="flex">
+      <article className="shrink-0 w-80 h-96 border-2 border-red-600 object-center overflow-hidden rounded-xl shadow-lg transition hover:shadow-2xl">
+        <img
+          alt="Office"
+          src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          className="h-56 w-full object-cover"
+        />
 
-      <div className="bg-white p-4 sm:p-6">
-        <time datetime={date} className="block text-xs text-gray-500">
-          {date}
-        </time>
+        <div className="bg-white p-4 sm:p-6">
+          <time datetime={date} className="block text-xs text-gray-500">
+            {date}
+          </time>
 
-        <a href={slug}>
-          <h3 className="mt-0.5 text-lg text-gray-900">
-            {blogtitle}
-          </h3>
-        </a>
+          <a href={`/blog/${slug}`}>
+            <h3 className="mt-0.5 text-lg text-gray-900">{blogtitle}</h3>
+          </a>
 
-        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-          {content}
-        </p>
-      </div>
-    </article>
+          <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+            {content}
+          </p>
+        </div>
+      </article>
+    </div>
   );
-}
+};
 
 export default BlogCards;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,16 +10,18 @@ const BlogPage = ({ data }) => {
     <html>
       <Header></Header>
 
-      <div className="grid grid-cols-4 gap-4 mx-40 my-20">
-        {data.allMdx.nodes.map((node) => (
-          <BlogCards
-            blogtitle={node.frontmatter.title}
-            content={node.excerpt}
-            date={node.frontmatter.date}
-            slug={node.frontmatter.slug}
-          ></BlogCards>
-          // <li key={node.name}>{node.name}</li>
-        ))}
+      <div className="md:mx-auto">
+        <div className="grid grid-flow-col auto-cols-min md:grid-cols-4 md:auto-rows-max gap-4 mx-40 my-20">
+          {data.allMdx.nodes.map((node) => (
+            <BlogCards
+              blogtitle={node.frontmatter.title}
+              content={node.excerpt}
+              date={node.frontmatter.date}
+              slug={node.frontmatter.slug}
+            ></BlogCards>
+            // <li key={node.name}>{node.name}</li>
+          ))}
+        </div>
       </div>
 
       <Footer></Footer>
