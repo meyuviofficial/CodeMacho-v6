562005dc669c4b0d48136c473e4a3d872cc1b02e
Yuvi <Yuvaraj.Selvarajan@shell.com>
2023-04-28 14:29:49 +0530

    added border radius

--- src/components/header.js
+++ src/components/header.js
@@ -2,47 +2,47 @@ import React from "react";
 import { useState } from "react";
 
 function Header() {
-    const [state, setState] = useState(false);
+  const [state, setState] = useState(false);
 
-    // Replace javascript:void(0) path with your path
-    const navigation = [
-      { title: "Home", path: "javascript:void(0)" },
-      { title: "Blog", path: "javascript:void(0)" },
-      { title: "About", path: "javascript:void(0)" },
-      { title: "Contact", path: "javascript:void(0)" },
-      { title: "Projects", path: "javascript:void(0)" },
-    ];
-      return (
-        <nav className="bg-red-600 w-full border-b md:border-0 md:static">
-          <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-            <div className="flex items-center justify-start py-3 md:py-5 md:block">
-              <a href="javascript:void(0)">
-                <img
-                  src="https://www.floatui.com/logo.svg"
-                  width={120}
-                  height={50}
-                  alt="Float UI logo"
-                />
-              </a>
-            </div>
-            <div
-              className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                state ? "block" : "hidden"
-              }`}
-            >
-              <ul className="justify-end items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                {navigation.map((item, idx) => {
-                  return (
-                    <li key={idx} className="text-slate hover:text-indigo-600">
-                      <a href={item.path}>{item.title}</a>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-        </nav>
-      );
+  // Replace javascript:void(0) path with your path
+  const navigation = [
+    { title: "Home", path: "javascript:void(0)" },
+    { title: "Blog", path: "javascript:void(0)" },
+    { title: "About", path: "javascript:void(0)" },
+    { title: "Contact", path: "javascript:void(0)" },
+    { title: "Projects", path: "javascript:void(0)" },
+  ];
+  return (
+    <nav className="bg-red-600 fixed w-full border-b md:border-0 md:static">
+      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
+        <div className="flex items-center justify-start py-3 md:py-5 md:block">
+          <a href="javascript:void(0)">
+            <img
+              src="https://www.floatui.com/logo.svg"
+              width={120}
+              height={50}
+              alt="Float UI logo"
+            />
+          </a>
+        </div>
+        <div
+          className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            state ? "block" : "hidden"
+          }`}
+        >
+          <ul className="justify-end text-slate-200 items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {navigation.map((item, idx) => {
+              return (
+                <li key={idx} className="hover:text-gray-800">
+                  <a href={item.path}>{item.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
