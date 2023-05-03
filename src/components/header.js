a41e042744e1c6103e313b7a265200681459b395
Yuvi <Yuvaraj.Selvarajan@shell.com>
2023-05-04 00:29:56 +0530

    fixed the nav bar issue

--- src/components/header.js
+++ src/components/header.js
@@ -14,10 +14,10 @@ function Header() {
     { title: "Projects", path: "javascript:void(0)" },
   ];
   return (
-    <nav className="bg-red-600 w-screen">
+    <nav className="bg-red-600 w-full border-b md:border-0 md:static">
       <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-        <div className="flex items-center justify-start py-3 md:py-5 md:block">
-          <a href="javascript:void(0)">
+        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+          <a href="/">
             <img
               src="https://www.floatui.com/logo.svg"
               width={120}
@@ -25,17 +25,56 @@ function Header() {
               alt="Float UI logo"
             />
           </a>
+          <div className="md:hidden">
+            <button
+              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
+              onClick={() => setState(!state)}
+            >
+              {state ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
         <div
-          className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
             state ? "block" : "hidden"
           }`}
         >
-          <ul className="justify-end text-slate-200 items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+          <ul className="justify-end items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
             {navigation.map((item, idx) => {
               return (
-                <li key={idx} className="hover:text-gray-800">
-                  <Link to={item.path} activeClassName="active text-gray-800">
+                <li key={idx} className="text-slate-200 hover:text-gray-800">
+                  <Link
+                    to={item.path}
+                    activeClassName="block active text-gray-800"
+                  >
                     {item.title}
                   </Link>
                 </li>
