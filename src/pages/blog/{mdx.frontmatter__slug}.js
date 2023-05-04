427ea4d6618b5387b860f68c92462010626d49ee
Yuvi <Yuvaraj.Selvarajan@shell.com>
2023-05-04 23:44:12 +0530

    fixed the alignment issue

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -7,10 +7,10 @@ const BlogPost = ({ data, children }) => {
   return (
     <html lang="en">
       <Layout>
-        <div class="flex items-center justify-center min-h-screen min-w-screen md:w-full px-1">
-          <div className="shrink-0 justify-center drop-shadow-2xl p-8 mt-12 mx-24 w-screen md:w-3/5 border-2 border-red-600 bg-gradient-to-br from-gray-100 to-gray-300 rounded-xl prose-headings:text-red-600">
+        <div class="flex items-center justify-center min-h-screen min-w-screen md:w-full px-2 md:px-8">
+          <div className="shrink-0 justify-center drop-shadow-2xl p-8 mt-12 mx-24 w-full md:w-max border-2 border-red-600 bg-gradient-to-br from-gray-100 to-gray-300 rounded-xl prose-headings:text-red-600">
             <article className="prose lg:prose-l prose-img:rounded-xl prose-a:text-blue-600">
-              <h1 className="w-fit justify-center mb-4 pt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              <h1 className="mb-4 pt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                 {data.mdx.frontmatter.title}
               </h1>
               <p className="font-bold text-red-600">
